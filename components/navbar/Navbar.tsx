0a313edb77e857af588756f4f3408c2012669316
Javier Monardo <javorengero2000@yahoo.com.ar>
2024-03-30 15:09:00 -0300

    Refactoring Navbar component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Link from "next/link";
+import { HomeIcon } from "@primer/octicons-react";
+
+type navItemType = {
+  path: string;
+  text: string;
+};
+
+const navItems: navItemType[] = [
+  { path: "/about", text: "About" },
+  { path: "/pricing", text: "Pricing" },
+  { path: "/contact", text: "Contact" },
+];
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
+      <Link href="/">
+        <HomeIcon className="mx-2" size={24} />
+        Home
+      </Link>
       <span>
-        <Link href="/">Home</Link>
-      </span>
-      <span>
-        <Link className="mr-2" href="/about">
+        {/* <Link className="mr-2" href="/about">
           About
         </Link>
         <Link className="mr-2" href="/contact">
@@ -16,7 +29,14 @@ const Navbar = () => {
         </Link>
         <Link className="mr-2" href="/pricing">
           Pricing
-        </Link>
+        </Link> */}
+        {navItems.map(
+          (navItem: navItemType): ReactNode => (
+            <Link key={navItem.path} href={navItem.path} className="mr-2">
+              {navItem.text}
+            </Link>
+          )
+        )}
       </span>
     </nav>
   );
