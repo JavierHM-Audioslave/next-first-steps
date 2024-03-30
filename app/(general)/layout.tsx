1235bf636ff2a879466e44a190b238ead3e56116
Javier Monardo <javorengero2000@yahoo.com.ar>
2024-03-30 13:07:34 -0300

    Navbar created and rendered in the components AboutPage, PricingPage and ContactPage. This behaviour is achieved by simply rendering this new Navbar component in the GeneralLayout.

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,12 +1,17 @@
+import Navbar from "@/components/navbar/Navbar";
+
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col items-center py-24">
-      <h1>Hola Mundo</h1>
-      {children}
-    </div>
+    <>
+      <Navbar />
+      <div className="min-h-screen flex flex-col items-center py-24">
+        <h1>Hola Mundo</h1>
+        {children}
+      </div>
+    </>
   );
 }
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,8 +2,21 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span>Home</span>
+    <nav className="flex justify-between bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
+      <span>
+        <a href="/">Home</a>
+      </span>
+      <span>
+        <a className="mr-2" href="/about">
+          About
+        </a>
+        <a className="mr-2" href="/contact">
+          Contact
+        </a>
+        <a className="mr-2" href="/pricing">
+          Pricing
+        </a>
+      </span>
     </nav>
   );
 };
